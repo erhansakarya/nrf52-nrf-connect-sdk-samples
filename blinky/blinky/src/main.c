21a8118beb19933f42e1527eff51064558c5a052
erhan sakarya <erhansakaryaa@gmail.com>
2024-02-12 21:34:13 +0300

    tested gpio input interrupt.

--- blinky/blinky/src/main.c
+++ blinky/blinky/src/main.c
@@ -8,7 +8,7 @@
 #include <zephyr/drivers/gpio.h>
 
 /* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   100
+#define SLEEP_TIME_MS   (10*60*1000)
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
@@ -22,6 +22,13 @@
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
+
+void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+    gpio_pin_toggle_dt(&led);
+}
+
+static struct gpio_callback button_cb_data;
 
 int main(void)
 {
@@ -45,10 +52,18 @@ int main(void)
 		return -1;
 	}
 
+ 	ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+
+ 	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+
+ 	gpio_add_callback(button.port, &button_cb_data);
+
 	while (1) {
+#if 0
 		bool val = gpio_pin_get_dt(&button);
 
 		gpio_pin_set_dt(&led,val);
+#endif
 	#if 0
 		ret = gpio_pin_toggle_dt(&led);
 		if (ret < 0) {
